c5d905eb6869a86e672ad554fa974072d81f0065
jose Brusa <josebrusa@outlook.com>
2021-04-05 17:30:33 -0300

    hooks

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -6,7 +6,18 @@ import confLogo from '../componets/images/platziconf-logo.svg';
 import Badge from '../componets/Badge';
 import DeleteBadgeModal from '../componets/DeleteBadgeModal';
 
+
+function useIncreaseCount(max) {
+    const [ count, setCount ] = React.useState(0);
+
+    if (count > max) {
+        setCount(0);
+    }
+    return [count, setCount]
+}
+
 function BadgeDetails(props) {
+    const [ count, setCount ] = useIncreaseCount(4);
     const badge = props.badge;
 
     return (
@@ -41,6 +52,11 @@ function BadgeDetails(props) {
                 <h2>Actions</h2>
                 <div>
                 <div>
+                <button onClick={()=>{
+                    setCount(count + 1);
+                }} className="btn btn-primary mr-4">
+                Increase Count: { count }
+                </button>
                     <Link
                     className="btn btn-primary mb-4"
                     to={`/badges/${badge.id}/edit`}
